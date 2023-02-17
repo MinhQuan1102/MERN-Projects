d740f2a7729683bc2c3a9be2ce97d5b713d17576
Minh Quan <steaky37@gmail.com>
2023-02-17 10:17:48 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Homepage from "./pages/Homepage/Homepage";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="body">
+        <Route path="/" exact component={Homepage} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BreadCrumb from "../../components/BreadCrumb/BreadCrumb"
+const Login = () => {
+  return (
+    <div>
+      <BreadCrumb title="Login" />
+      
+    </div>
+  );
+};
+
+export default Login;
