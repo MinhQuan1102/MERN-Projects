fe8c87793b001f42eae1beb289f59a99dc3beb5e
Cá Côn <115128671+VietHoa58@users.noreply.github.com>
2023-02-23 00:41:14 +0700

    update 23/2/2023

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,11 +1,44 @@
 import React from "react";
-import BreadCrumb from "../../components/BreadCrumb/BreadCrumb"
+import "./login.css"
+import { Link } from "react-router-dom";
+import BreadCrumb from "../../components/BreadCrumb/BreadCrumb";
+import Container from "../../components/Container/Container";
+import CustomInput from "../../components/CustomInput/CustomInput";
+
 const Login = () => {
   return (
-    <div>
+    <>
       <BreadCrumb title="Login" />
-      
-    </div>
+      <Container class1="login-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <div className="auth-card">
+              <h3 className="text-center mb-3">Login</h3>
+              <form action="" className="d-flex flex-column gap-15">
+                <CustomInput type="email" name="email" placeholder="Email" />
+                <CustomInput
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
+                <div>
+                  <Link to="/forgot-password">Forgot Password?</Link>
+
+                  <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
+                    <button className="button border-0" type="submit">
+                      Login
+                    </button>
+                    <Link to="/Register" className="button signup">
+                      SignUp
+                    </Link>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 };
 
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import "./register.css"
-
+import React from "react";
+import "./register.css";
+import BreadCrumb from "../../components/BreadCrumb/BreadCrumb";
+import { Link } from "react-router-dom";
+import Container from "../../components/Container/Container";
+import CustomInput from "../../components/CustomInput/CustomInput";
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <>
+      <BreadCrumb title="Register" />
+      <Container class1="login-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <div className="auth-card">
 
-export default Register
+              <h3 className="text-center mb-3">Register</h3>
+              <form action="" className="d-flex flex-column gap-15">
+                <CustomInput type="text" name="name" placeholder="Name" />
+                <CustomInput type="email" name="email" placeholder="Email" />
+                <CustomInput type="tel" name="mobile" placeholder="Mobile Number"/>
+                <CustomInput type="password"name="password" placeholder="Password"/>
+                <CustomInput type="password"name="confirmPassword" placeholder="Confirm Password"/>
+                
+                <div>
+                  <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
+                    <button className="button border-0">Register</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Register;
